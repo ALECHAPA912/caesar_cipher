0c1ffd1376915bc6d45b39d2581b893faf31b972
ALECHAPA912 <chapale2000@gmail.com>
2025-12-01 17:53:53 -0300

    rspec intialized, main.rb file created, attr accesor added, change key and change phrase added

--- caesar.rb
+++ caesar.rb
@@ -1,22 +1,30 @@
-def caesar_cipher(string, key)
-  alphabet = ('a'..'z').to_a
-  result = string.split("")
-  result.each_with_index do |letter, i|
-    alphabet.each_with_index do |char, index|
-      pos = index + key
-      if (char == letter)
-        result[i] = alphabet[pos]
-        if (pos > 25)
-          result[i] = alphabet[pos - 26]
+class CaesarCipher
+
+  def initialize(string, key)
+    @string = string
+    @key = key
+  end
+
+  def caesar_cipher
+    alphabet = ('a'..'z').to_a
+    result = @string.split("")
+    result.each_with_index do |letter, i|
+      alphabet.each_with_index do |char, index|
+        pos = index + @key
+        if (char == letter)
+          result[i] = alphabet[pos]
+          if (pos > 25)
+            result[i] = alphabet[pos - 26]
+          end
+        end
+        if (char.upcase == letter)
+          result[i] = alphabet[pos].upcase
+          if (pos > 25)
+            result[i] = alphabet[pos - 26]
+          end
         end
       end
-      if (char.upcase == letter)
-        result[i] = alphabet[pos].upcase
-        if (pos > 25)
-          result[i] = alphabet[pos - 26]
-        end
-      end
-    end
   end
   print result.join("")
+  end
 end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require './caesar.rb'
+
+phrase = CaesarCipher.new('Pinga#', 1)
+puts phrase.caesar_cipher
